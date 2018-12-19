8a8a81cd20457f290d43447771755891ac51c2aa
BT_1N3_03@BKTECH.LOCAL <BT_1N3_03@BKTECH.LOCAL>
2018-12-19 11:43:32 -0500

    Initial commit

---
+++ src/com/company/Position.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Position {
+    public int r, c;
+
+    public Position(int row, int column) {
+        this.r = row;
+        this.c = column;
+    }
+}
---
+++ src/com/company/Runner.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        /* The example array */
+        int[][] coolArray = {
+                {10, 15, 13, 8},
+                {12, 6, 9, 7},
+                {17, 11, 16, 14}
+        };
+
+        /*
+        Position partA = Successors.findPosition(7, coolArray);
+        System.out.println("Row of 7: " + partA.r);
+        System.out.println("Column of 7: " + partA.c);
+        */
+
+        Position[][] partB = Successors.getSuccessorArray(coolArray);
+
+        for (int i = 0; i < partB.length; i ++) {
+            for (int j = 0; j < partB[i].length; j ++) {
+                if (partB[i][j] != null) {
+                    System.out.print(("(" + partB[i][j].r + ", " + partB[i][j].c + ")") + "; ");
+                }
+                else {
+                    System.out.print("null" + "; ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
